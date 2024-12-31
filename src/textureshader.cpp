b58536957b3b3631d12d42e5aa6ef1ed104c0557
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2024-12-31 14:45:50 -0500

    more shaderwork changes

--- src/textureshader.cpp
+++ src/textureshader.cpp
@@ -1,34 +1,237 @@
 #include "textureshader.h"
 
-bool ShadedTexture::LoadPixelsFromFile(const char *path)
+ShadedTexture::ShadedTexture()
 {
-    //Free preexisting assets
-    free();
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
 
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load( path );
-    if( loadedSurface == NULL )
-    {
-        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-    }
-    else
-    {
-        //Convert surface to display format
-        mSurfacePixels = SDL_ConvertSurfaceFormat( loadedSurface, SDL_GetWindowPixelFormat( gWindow ), 0 );
-        if( mSurfacePixels == NULL )
-        {
-            printf( "Unable to convert loaded surface to display format! SDL Error: %s\n", SDL_GetError() );
-        }
-        else
-        {
-            //Get image dimensions
-            mWidth = mSurfacePixels->w;
-            mHeight = mSurfacePixels->h;
-        }
+	mSurfacePixels = NULL;
+}
 
-        //Get rid of old loaded surface
-        SDL_FreeSurface( loadedSurface );
-    }
+ShadedTexture::~ShadedTexture()
+{
+	//Deallocate
+	Release();
+}
 
-    return mSurfacePixels != NULL;
+bool ShadedTexture::LoadFromFile( const char *path )
+{
+	//Load pixels
+	if( !LoadPixelsFromFile( path ) )
+	{
+		printf( "Failed to load pixels for %s!\n", path );
+	}
+	else
+	{
+		//Load texture from pixels
+		if( !LoadFromPixels() )
+		{
+			printf( "Failed to texture from pixels from %s!\n", path );
+		}
+	}
+
+	//Return success
+	return mTexture != NULL;
+}
+
+
+bool ShadedTexture::LoadPixelsFromFile( const char *path )
+{
+	//Free preexisting assets
+	free();
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+	}
+	else
+	{
+		//Convert surface to display format
+		mSurfacePixels = SDL_ConvertSurfaceFormat( loadedSurface, SDL_GetWindowPixelFormat( gWindow ), 0 );
+		if( mSurfacePixels == NULL )
+		{
+			printf( "Unable to convert loaded surface to display format! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = mSurfacePixels->w;
+			mHeight = mSurfacePixels->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	return mSurfacePixels != NULL;
+}
+
+bool ShadedTexture::LoadFromPixels(SDL_Renderer *renderer)
+{
+	//Only load if pixels exist
+	if( mSurfacePixels == NULL )
+	{
+		printf( "No pixels loaded!" );
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey( mSurfacePixels, SDL_TRUE, SDL_MapRGB( mSurfacePixels->format, 0, 0xFF, 0xFF ) );
+
+		//Create texture from surface pixels
+		mTexture = SDL_CreateTextureFromSurface( renderer, mSurfacePixels );
+		if( mTexture == NULL )
+		{
+			printf( "Unable to create texture from loaded pixels! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = mSurfacePixels->w;
+			mHeight = mSurfacePixels->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( mSurfacePixels );
+		mSurfacePixels = NULL;
+	}
+
+	//Return success
+	return mTexture != NULL;
+}
+
+#if defined(SDL_TTF_MAJOR_VERSION)
+bool ShadedTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+	if( textSurface != NULL )
+	{
+		//Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+		if( mTexture == NULL )
+		{
+			printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		//Get rid of old surface
+		SDL_FreeSurface( textSurface );
+	}
+	else
+	{
+		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
+	}
+
+	
+	//Return success
+	return mTexture != NULL;
+}
+#endif
+
+void ShadedTexture::Release()
+{
+	//Free texture if it exists
+	if( mTexture != NULL )
+	{
+		SDL_DestroyTexture( mTexture );
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+
+	//Free surface if it exists
+	if( mSurfacePixels != NULL )
+	{
+		SDL_FreeSurface( mSurfacePixels );
+		mSurfacePixels = NULL;
+	}
+}
+
+void ShadedTexture::SetColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+	//Modulate texture rgb
+	SDL_SetTextureColorMod( mTexture, red, green, blue );
+}
+
+void ShadedTexture::SetBlendMode( SDL_BlendMode blending )
+{
+	//Set blending function
+	SDL_SetTextureBlendMode( mTexture, blending );
+}
+		
+void ShadedTexture::SetAlpha( Uint8 alpha )
+{
+	//Modulate texture alpha
+	SDL_SetTextureAlphaMod( mTexture, alpha );
+}
+
+void ShadedTexture::Render(SDL_Renderer *renderer, int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+	//Set clip rendering dimensions
+	if( clip != NULL )
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	//Render to screen
+	SDL_RenderCopyEx( renderer, mTexture, clip, &renderQuad, angle, center, flip );
+}
+
+SDL_Point ShadedTexture::GetSize()
+{
+    return {mWidth, mHeight};
+}
+
+Uint32* ShadedTexture::GetPixels32()
+{
+	Uint32* pixels = NULL;
+
+	if( mSurfacePixels != NULL )
+	{
+		pixels =  static_cast<Uint32*>( mSurfacePixels->pixels );
+	}
+
+	return pixels;
+}
+
+Uint32 ShadedTexture::GetPitch32()
+{
+	Uint32 pitch = 0;
+
+	if( mSurfacePixels != NULL )
+	{
+		pitch = mSurfacePixels->pitch / 4;
+	}
+
+	return pitch;
+}
+
+
+Uint32 ShadedTexture::MapRGBA( Uint8 r, Uint8 g, Uint8 b, Uint8 a )
+{
+	Uint32 pixel = 0;
+
+	if( mSurfacePixels != NULL )
+	{
+		pixel = SDL_MapRGBA( mSurfacePixels->format, r, g, b, a );
+	}
+
+	return pixel;
 }
