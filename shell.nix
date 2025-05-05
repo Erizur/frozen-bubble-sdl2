f48f31e406efefec8c9d5cfa6a033dae73189ecd
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2025-05-05 14:06:36 -0500

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,23 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-   buildInputs = [
-     pkgs.clang
-     pkgs.clang-tools
-     pkgs.lldb
-     pkgs.ninja 
-     pkgs.cmake
-
-     pkgs.SDL2
-     pkgs.SDL2_mixer
-     pkgs.SDL2_ttf
-     pkgs.SDL2_image
-     pkgs.iniparser
-     pkgs.pkg-config
-     pkgs.glib
-
-     # Original game for development purposes
-     pkgs.frozen-bubble
-   ];
-  # Use clang's stdenv
-  stdenv = pkgs.llvmPackages_latest.stdenv;
+  inputsFrom = [(pkgs.callPackage ./default.nix {})];
 }
